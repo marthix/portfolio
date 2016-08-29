2f496d6cce29518751b453a516ebd4d2973e4da6
Jason Lestina <coda19@gmail.com>
2016-08-28 20:53:09 -0400

    cleaning up files for portfolio

--- sockapalooza-master/js/filters.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-
-  $('.filters')
-    .on('click', function() {
-      $('.filters-arrow', this).toggleClass('flip')
-    })
-
-})
--- sockapalooza/js/thankyou.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-	$(".btn-send").click(function(){
-		$("#thankYouModal").modal('show');
-	});
-});

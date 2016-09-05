1b51bbad17768e06cc08d0e4b099f6ea7d224d55
Jason Lestina <coda19@gmail.com>
2016-09-05 16:05:04 -0400

    fixing logo flexing/mobile

--- js/main.js
+++ js/main.js
@@ -9,30 +9,45 @@ var featured = qs('#featured')
 
 $(document).ready(function(){
   // Make nav work
-
+  log($(window).width())
 
   // On document scroll, change the navbar backround color
-   var scroll_start = 0;
-   var startchange = $('.nav-container');
-   var offset = startchange.offset();
+  var scroll_start = 0
+  var startchange = $('.nav-container')
+  var offset = startchange.offset()
 
-   $(document).scroll(function() {
-      scroll_start = $(this).scrollTop();
-      if(scroll_start > offset.top + 50) {
-        $('.nav-container').css('background-color', '#FCFC62');
-        $('li>a').css('color', '#343431');
-        $('.logo').css('background-color', '#343431')
+  $(document).scroll(function() {
+    scroll_start = $(this).scrollTop()
+    if(scroll_start > offset.top + 50) {
+      $('.nav-container').css('background-color', '#FCFC62')
+      $('li>a').css('color', '#343431')
+      $('.logo').css('background-color', '#343431')
+      $('.logo').css('width', '35px')
+      $('.logo').css('height', '50px')
+    } else {
+      $('.nav-container').css('background-color', '#343431')
+      $('li>a').css('color', '#FCFC62')
+      $('.logo').css('background-color', '#FCFC62')
+      if ($(window).width() < 425) {
         $('.logo').css('width', '35px')
         $('.logo').css('height', '50px')
-       } else {
-         $('.nav-container').css('background-color', '#343431');
-         $('li>a').css('color', '#FCFC62');
-         $('.logo').css('background-color', '#FCFC62')
-         $('.logo').css('width', '70px')
-         $('.logo').css('height', '100px')
-       }
-   });
-});
+      } else {
+        $('.logo').css('width', '70px')
+        $('.logo').css('height', '100px')
+      }
+    }
+  })
+
+  $(window).resize(function() {
+    if ($(window).width() < 425) {
+      $('.logo').css('width', '35px')
+      $('.logo').css('height', '50px')
+    } else {
+      $('.logo').css('width', '70px')
+      $('.logo').css('height', '100px')
+    }
+  })
+})
 
 document.body.addEventListener('click', function(e){
   if (e.target.getAttribute('href').includes('#')) {
